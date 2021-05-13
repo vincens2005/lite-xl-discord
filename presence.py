01991b28b47aaacfc0cc1269f660747dd598f6bd
cukmekerb <cukmekerb@gmail.com>
2021-05-13 12:57:35 -0700

    add pidfile so that multiple instances of the presence.py don't run

--- presence.py
+++ presence.py
@@ -19,11 +19,22 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--pickle", dest="pickle")
 args = parser.parse_args()
 
+
+current_pid = str(os.getpid())
+pidfile = "presence_py.pid"
+
+if os.path.isfile(pidfile):
+    print("other instance is running. I will go commit die.")
+    exit()
+open(pidfile, 'w').write(current_pid)  # write pid to file
+
+
 def load_data(cur_dat, pickle_file):
     fp = open(pickle_file, "rb")
     data = pickle.load(fp)
     data = json.loads(data)
     if data["die_now"] != "no":
+        os.unlink(pidfile)  # delete file before killing self
         exit()
     if data != cur_dat:
         print("got new data!")
@@ -42,24 +53,27 @@ def reset_data(data, pickle_file):
     fp.close()
 
 
-# discord client id:
-client_id = "839231973289492541"
-rpc = Presence(client_id)
-print("connecting to rpc...")
-rpc.connect()
-start_time = time.time()
-rpc.update(start=start_time, state="loading discord plugin", large_image="lite-xl")
-
-reset_data(discord_data, args.pickle)
-
-while True:
-    print("current discord dta:", discord_data)
-    discord_data = load_data(discord_data, args.pickle)
-    rpc.update(
-        pid=os.getpid(),
-        start=start_time,
-        state=discord_data["state"],
-        details=discord_data["details"],
-        large_image="lite-xl"
-    )
-    time.sleep(15)  # discord sleep timeout
+try:
+    # discord client id:
+    client_id = "839231973289492541"
+    rpc = Presence(client_id)
+    print("connecting to rpc...")
+    rpc.connect()
+    start_time = time.time()
+    rpc.update(start=start_time, state="loading discord plugin", large_image="lite-xl")
+    
+    reset_data(discord_data, args.pickle)
+    
+    while True:
+        print("current discord dta:", discord_data)
+        discord_data = load_data(discord_data, args.pickle)
+        rpc.update(
+            pid=os.getpid(),
+            start=start_time,
+            state=discord_data["state"],
+            details=discord_data["details"],
+            large_image="lite-xl"
+        )
+        time.sleep(15)  # discord sleep timeout
+finally:
+    os.unlink(pidfile)  # delete pidfile if crash happens
